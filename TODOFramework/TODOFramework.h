5a21ebf71d7bb49e10c0db40f72f4d3658230ee7
Tu Nguyen <tunt.dl3@gmail.com>
2024-01-22 15:37:50 +0700

    init

---
+++ TODOFramework/TODOFramework.h
@@ -0,0 +1,18 @@
+//
+//  TODOFramework.h
+//  TODOFramework
+//
+//  Created by Tu Nguyen on 22/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TODOFramework.
+FOUNDATION_EXPORT double TODOFrameworkVersionNumber;
+
+//! Project version string for TODOFramework.
+FOUNDATION_EXPORT const unsigned char TODOFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TODOFramework/PublicHeader.h>
+
+
---
+++ TODOFramework/TODOView.swift
@@ -0,0 +1,12 @@
+//
+//  TODOView.swift
+//  TODOFramework
+//
+//  Created by Tu Nguyen on 22/01/2024.
+//
+
+import Foundation
+public class TODOView {
+    public var name = "to do"
+    init() {}
+}
